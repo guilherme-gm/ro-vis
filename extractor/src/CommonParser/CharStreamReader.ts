03d223073b42fcf08d071d84fb373bb78c230d14
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2024-05-05 09:36:52 -0300

    add character stream reader

---
+++ extractor/src/CommonParser/CharStreamReader.ts
@@ -0,0 +1,67 @@
+export class CharStreamReader {
+	private readonly text: string = '';
+
+	private lineNumber: number = 1;
+
+	private pointer: number = 0;
+
+	constructor (text: string) {
+		this.text = text.replace(/\r/g, "").replace(/\u3000/g, "");
+	}
+
+	public isEndOfFile(): boolean {
+		return this.pointer >= this.text.length;
+	}
+
+	public isLineStart(): boolean {
+		if (this.pointer == 0) {
+			return true;
+		}
+
+		if (this.text[this.pointer - 1] === '\n') {
+			return true;
+		}
+
+		return false;
+	}
+
+	public getLineNumber(): number {
+		return this.lineNumber;
+	}
+
+	public readChar(): string {
+		if (this.isEndOfFile()) {
+			throw new Error('End of file reached.');
+		}
+
+		const char = this.text[this.pointer]!;
+		this.pointer++;
+
+		if (char === '\n') {
+			this.lineNumber++;
+		}
+
+		return char;
+	}
+
+	public peekChar(): string | null {
+		if (this.isEndOfFile()) {
+			return null;
+		}
+
+		return this.text[this.pointer]!;
+	}
+
+	public unreadChar(): void {
+		if (this.pointer == 0) {
+			throw new Error('Cannot unread. Already at the beggining of the file.');
+		}
+
+		this.pointer--;
+		const char = this.text[this.pointer];
+
+		if (char === '\n') {
+			this.lineNumber--;
+		}
+	}
+}
