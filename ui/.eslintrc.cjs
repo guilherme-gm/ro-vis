72be22fc81d8b820a5aa4beb70912006d7f3165a
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2024-05-18 00:56:46 -0300

    initial vue project

---
+++ ui/.eslintrc.cjs
@@ -0,0 +1,23 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+	root: true,
+	'extends': [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-typescript'
+	],
+	parserOptions: {
+		ecmaVersion: 'latest'
+	},
+	rules: {
+		"vue/html-indent": ["error", 'tab', {
+			"attribute": 1,
+			"baseIndent": 1,
+			"closeBracket": 0,
+			"alignAttributesVertically": true,
+			"ignores": []
+		}]
+	},
+}
