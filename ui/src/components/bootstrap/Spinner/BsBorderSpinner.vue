d2073ea32528e7d476579658f8ebcf6ceae29f96
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2024-05-19 13:43:31 -0300

    add bootstrap border spinner

---
+++ ui/src/components/bootstrap/Spinner/BsBorderSpinner.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { ThemeColors } from '../ThemeColors';
+
+const props = withDefaults(
+	defineProps<{
+		color?: ThemeColors;
+		message?: string;
+	}>(),
+	{
+		color: ThemeColors.Primary,
+		message: 'Loading...',
+	},
+);
+
+const textClass = computed(() => `text-${props.color}`);
+</script>
+
+<template>
+	<div class="spinner-border" role="status" :class="{ [textClass]: true }">
+		<span class="visually-hidden">{{ message }}</span>
+	</div>
+</template>
