2078a790cda1716fb221d97ea4999b3efc029538
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2024-05-22 22:42:06 -0300

    add pagination component

---
+++ ui/src/components/bootstrap/Pagination/BsPagination.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+	pages: number[];
+	currentPage: number;
+	description: string;
+	center?: boolean;
+}>();
+
+const emit = defineEmits(['changed']);
+</script>
+
+<template>
+	<nav :aria-label="description">
+		<ul class="pagination" :class="{ 'justify-content-center': center }">
+			<li
+				v-for="(page) in pages"
+				:key="`page-${page}`"
+				class="page-item"
+				:class="{ active: (page === currentPage) }"
+			>
+				<button class="page-link" @click="emit('changed', page)">{{ page }}</button>
+			</li>
+		</ul>
+	</nav>
+</template>
