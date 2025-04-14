2d5b92195b78810aaf9a76326978e8f7c519632e
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2025-04-14 08:47:35 -0300

    Initial go extractor module

---
+++ extractor-go/cmd/dev/dev.go
@@ -0,0 +1,12 @@
+/**
+ * "Development" entry point
+ * This has limited scope to be used while developing something instead of running the entire history
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("RO Vis extractor - DEV")
+}
---
+++ extractor-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/guilherme-gm/ro-vis/extractor
+
+go 1.24.2
