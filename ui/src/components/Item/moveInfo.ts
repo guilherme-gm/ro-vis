fbe457435aaf6e9ab33fbe7729aad3b8b18ca06e
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2025-07-02 08:26:43 -0300

    fix MoveInfo field names

--- ui/src/components/Item/moveInfo.ts
+++ ui/src/components/Item/moveInfo.ts
@@ -4,14 +4,14 @@ import type { Component } from "vue";
 import AuctionIcon from "../icons/AuctionIcon.vue";
 
 const fields: { key: keyof ItemMoveInfo; label: string; icon: Component; }[] = [
-	{ key: "canDrop", label: "Drop", icon: BIconTrash },
-	{ key: "canMail", label: "Mail", icon: BIconEnvelopePlus },
-	{ key: "canMoveToCart", label: "Move to cart", icon: BIconCart },
-	{ key: "canMoveToStorage", label: "Move to storage", icon: BIconArchive },
-	{ key: "canMoveToGuildStorage", label: "Move to GUILD storage", icon: BIconBoxes },
-	{ key: "canSellToNpc", label: "Sell to NPC", icon: BIconCurrencyDollar },
-	{ key: "canTrade", label: "Trade", icon: BIconArrowLeftRight },
-	{ key: "canAuction", label: "Auction", icon: AuctionIcon },
+	{ key: "CanDrop", label: "Drop", icon: BIconTrash },
+	{ key: "CanMail", label: "Mail", icon: BIconEnvelopePlus },
+	{ key: "CanMoveToCart", label: "Move to cart", icon: BIconCart },
+	{ key: "CanMoveToStorage", label: "Move to storage", icon: BIconArchive },
+	{ key: "CanMoveToGuildStorage", label: "Move to GUILD storage", icon: BIconBoxes },
+	{ key: "CanSellToNpc", label: "Sell to NPC", icon: BIconCurrencyDollar },
+	{ key: "CanTrade", label: "Trade", icon: BIconArrowLeftRight },
+	{ key: "CanAuction", label: "Auction", icon: AuctionIcon },
 ];
 
 export function useMoveInfo() {
